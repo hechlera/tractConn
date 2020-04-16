c8badbea6746bda17be4af23d8d113eba856c648
hechlera <hechler.a@gmail.com>
2020-04-16 16:09:38 +0200

    Added file and folder checks, error calls and minimal verbosity

--- mrtrix3_act.sh
+++ mrtrix3_act.sh
@@ -7,6 +7,7 @@
 ######################## TO DO ########################
 ### - add flags for pipeline options
 ###     - add option to keep/erase previous files
+###     - option to run parts of pipeline
 ### - more error checks and helpful error calls
 ### - include set -e again
 ### - add verbose option with output
@@ -15,6 +16,8 @@
 ### - subject list file call?
 ###	    - second call to the file? Does that work?
 ### - deringing and ROBLEX brain ext?
+###     - move BET to beginning of script to allow early inspection?
+###     - visual output into terminal possible? File? 
 
 
 Usage() {
@@ -67,6 +70,9 @@ fi
 # get csv list with subjects and sessions
 SUBJ_LIST=$1
 
+echo '############################'
+echo 'TractConn: Subject list read, searching for folders and files'
+
 # iterate through all subjects and sessions based on provided file
 while IFS=',' read SUBJECT SESSION; do
 
@@ -94,6 +100,8 @@ while IFS=',' read SUBJECT SESSION; do
 	if [[ -z DWI_FILE || -z BVEC_FILE || -z BVAL_FILE || -z JSON_FILE ]]; then
 		echo "ERROR: DWI data not complete. Original nii, bvec, bval ans json must be available"
 		exit 1
+	else
+		echo 'Necessary DWI data found. Beginning preprocessing of subject ${SUBJ_ID}'
 	fi
 
 	# Added -json_import to store all info in mif header
@@ -138,6 +146,14 @@ while IFS=',' read SUBJECT SESSION; do
 	DWI_PP=$(find ${DWI_PATH} -name "*dwi_dn-preproc.mif") 
 	DWI_BCOR="${DWI_PP%.mif}-bcor.mif"
 	dwibiascorrect ${DWI_PP} ${DWI_BCOR} -ants
+	
+	if [[ -z DWI_BCOR ]]; then
+		echo "ERROR: Pre-processing could not be completed"
+		exit 1
+		
+	else
+		echo "Pre-processing completed. Continuing with response function estimation and CSD"
+	fi
 
 	##############################################################################
 	### STEP 3: ESTIMATE RESPONSE FUNCTIONS
@@ -179,11 +195,25 @@ while IFS=',' read SUBJECT SESSION; do
 	dwi2fod csd ${DWI_BCOR} ${RFE_FILE} ${CSD} -mask ${DWI_MASK_D}
 	### verbose optional: check fiber orientation density image - mrview fod.mif -odf.load_sh fod.mif
 	
+	if [[ -z ${CSD} || -z ${DWI_MASK_D} ]]; then
+		echo "ERROR: CSD could not be completed. Check for average response function file. "
+		exit 1
+	else
+		echo "CSD completed for current subject ${SUBJ_ID}. Continuing with registration steps."
+	fi
+	
+	
 	##############################################################################
 	### STEP 5: T1 -> DWI registration
 	
 	# get files, prepare output names; transform DWI to nii for registration with FSL
 	MPRAGE_FILE=$(find ${ANAT_PATH} -name "*T1w*.nii.gz")
+	
+	if [ -z ${MPRAGE_FILE} ]; then
+		echo 'Error: T1 image not found.'
+		exit 1
+	fi
+	
 	MPRAGE_BET="${ANAT_PATH}/sub-${SUBJ_ID}_ses-${SES_ID}_T1w_bet.nii.gz"
 	DWI_FILE=$(find ${DWI_PATH} -name "*dwi_dn-preproc-bcor.mif")
 
@@ -196,6 +226,8 @@ while IFS=',' read SUBJECT SESSION; do
 	if [ ! -d ${ANAT_PATH}/registration_files ]; then
 		echo "Creating output folder for registration matrices"
 		mkdir ${ANAT_PATH}/registration_files
+	else
+		echo "WARNING: Folder for registration files already present. Old files should be deleted."
 	fi
 
 	REG_FOLDER=${ANAT_PATH}/registration_files
@@ -206,6 +238,7 @@ while IFS=',' read SUBJECT SESSION; do
 	# BET on T1
 	### WARNING: Standard BET parameters might be to restrictive, resulting in cut-off grey matter
 	### Test different fractional intensity values (0 permissive - 1 restrictive; 0.5 = default)
+	echo 'WARNING: FSL BET is performed with fractional intensity value of 0.2. The default of 0.5 often cuts into the cortex. Check images afterwards.'
 	robustfov -i ${MPRAGE_FILE} -r ${MPRAGE_FILE}
 	bet ${MPRAGE_FILE} ${MPRAGE_BET} -f 0.2 -R
 	MPRAGE_BET=$(find ${ANAT_PATH} -name "*T1w_bet.nii.gz")
@@ -217,12 +250,24 @@ while IFS=',' read SUBJECT SESSION; do
 	transformconvert ${REG_FOLDER}/T12DWI.mat ${MPRAGE_BET} ${DWI_3D} flirt_import ${REG_FOLDER}/T12DWI.mrtrix
 	mrtransform ${MPRAGE_BET} -linear ${REG_FOLDER}/T12DWI.mrtrix "${MPRAGE_BET%.nii.gz}_regDWI.nii.gz"
 	
+	MPRAGE_REG=$(find ${ANAT_PATH} -name "*regDWI.nii.gz")
+	if [ -z ${MPRAGE_REG} ]; then
+		echo 'ERROR: T1 to DWI registration was not completed.'
+		exit 1
+	else
+		echo 'T1 to DWI registration completed. Continuing with parcellation or atlas registration'
+	fi
+	
 	##############################################################################
 	### STEP 6: ATLAS REGISTRATION / T1 PARCELLATION
 	
-	MPRAGE_REG=$(find ${ANAT_PATH} -name "*regDWI.nii.gz")
 	ATLAS=$(find ${PROJECT_DIRECTORY}/derivatives/PARCELLATION -name "Schaefer*.nii.gz")
 	MNI2mm=$(find ${PROJECT_DIRECTORY}/derivatives/PARCELLATION -name "MNI152_T1_1mm_brain.nii.gz")
+	
+	if [ -z ${ATLAS} || -z ${MNI2mm} ];then
+		echo 'ERROR: Yeo400 atlas and/or MNI2mm template were not found.'
+		exit 1
+	fi
 
 	# get transformmatrix from MNI->Sub, apply to atlas file (default affine reg (dof 12))
 	flirt -in ${MNI2mm} -ref ${MPRAGE_REG} -omat ${REG_FOLDER}/MNI2SUB.mat
@@ -241,14 +286,25 @@ while IFS=',' read SUBJECT SESSION; do
 	MPRAGE_SEG=$(find ${ANAT_PATH} -name "*seg.nii.gz") 
 	5tt2gmwmi ${MPRAGE_SEG} ${MPRAGE_GMWMI}
 	
+	if [ -z ${MPRAGE_SEG} || -z ${MPRAGE_GMWMI} ];then
+		echo 'ERROR: Segmentation could not be completed'
+		exit 1
+	else
+		echo 'Segmentation completed. Continuing with ACT and connectome creation.'
+	fi
+	
 	##############################################################################
 	### STEP 8: ACT AND CONNECTOME CREATION
 	
 	# get files (original DWI only for naming), name output
 	DWI_FILE=$(find ${DWI_PATH} -name "*dwi.mif")
+	# fod file already in ${CSD}
 	FOD_FILE=$(find ${DWI_PATH} -maxdepth 1 -name "*dwi_fod.mif")
+	# this apparently includes a step before that converts all values to integer. Do testing.
 	PARC_FILE=$(find ${ANAT_PATH} -name "Yeo400_reg_int.nii.gz")
+	# Seg already in ${MPRAGE_SEG}
 	SEG_FILE=$(find ${ANAT_PATH} -name "*T1w*seg.nii.gz")
+	# GMWMI already in ${MPRAGE_GMWMI}
 	GMWMI_FILE=$(find ${ANAT_PATH} -name "*T1w*seg-gmwmi.nii.gz")
 
 	TCK_OUT="${DWI_FILE%dwi.mif}tracts.tck"
@@ -256,14 +312,14 @@ while IFS=',' read SUBJECT SESSION; do
 	CONN_OUT="${DWI_FILE%dwi.mif}connectome_zeros.csv"
 
 	# anatomically constrained tractography, seeding at GM-WM interface with 20m streamlines
-	tckgen ${FOD_FILE} ${TCK_OUT} -act ${SEG_FILE} -seed_gmwmi ${GMWMI_FILE} -backtrack -select 20M
+	tckgen ${CSD} ${TCK_OUT} -act ${MPRAGE_SEG} -seed_gmwmi ${MPRAGE_GMWMI} -backtrack -select 20M
 
 	echo "#########################################################"
 	echo "tckgen for ${SUBJ_ID} completed"
 	echo "#########################################################"
 
 	# filtering down to 1/4 - 5m streamlines
-	tcksift ${TCK_OUT} ${FOD_FILE} ${TCKSFT_OUT} -act ${SEG_FILE} -term_number 5m
+	tcksift ${TCK_OUT} ${CSD} ${TCKSFT_OUT} -act ${MPRAGE_SEG} -term_number 5m
 
 	echo "#########################################################"
 	echo "tcksift for ${SUBJ_ID} completed"
@@ -273,7 +329,7 @@ while IFS=',' read SUBJECT SESSION; do
 	tck2connectome -symmetric -zero_diagonal ${TCKSFT_OUT} ${PARC_FILE} ${CONN_OUT}
 
 	echo "#########################################################"
-	echo "connectome for ${SUBJ_ID} completed"
+	echo "connectome for ${SUBJ_ID} completed. Continuing with next subject"
 	echo "#########################################################"
 	
 done < $SUBJ_LIST
